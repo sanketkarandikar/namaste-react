0f9f588e801f352eed0eb076ecbc6af9725e6b4c
Sanket Karandikar <sanketkarandikar@gmail.com>
2023-11-07 22:14:42 +0530

    added react in the project

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
+ import React from "react";
+
+import ReactDOM from "react-dom/client"; 
  const head = React.createElement('h1', {}, 'Hello from react app!');
+ const root = ReactDOM.createRoot(document.getElementById("root"));
 
-        const root = ReactDOM.createRoot(document.getElementById("root"));
-
-        root.render(head);
+ root.render(head);
